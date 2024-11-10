094561595ceca55b52eb98b92f33b24ebbbc2c6c
reganputra <ramadhanregan334@gmail.com>
2024-11-10 08:53:12 +0700

    feat: implement RawQuery for sorting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ android {
 
 dependencies {
 
+    implementation(libs.lifecycle.livedata.ktx)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
--- app/src/main/java/com/dicoding/mystudentdata/MainActivity.kt
+++ app/src/main/java/com/dicoding/mystudentdata/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.PopupMenu
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -12,6 +14,7 @@ import com.dicoding.mystudentdata.adapter.StudentListAdapter
 import com.dicoding.mystudentdata.adapter.StudentWithCourseAdapter
 import com.dicoding.mystudentdata.adapter.UniversityAndStudentAdapter
 import com.dicoding.mystudentdata.databinding.ActivityMainBinding
+import com.dicoding.mystudentdata.helper.SortType
 
 class MainActivity : AppCompatActivity() {
 
@@ -39,10 +42,12 @@ class MainActivity : AppCompatActivity() {
         return when (item.itemId) {
             R.id.action_single_table -> {
                 getStudent()
+                showSortingOptionMenu(true)
                 return true
             }
             R.id.action_many_to_one -> {
                 getStudentAndUniversity()
+                showSortingOptionMenu(false)
                 true
             }
             R.id.action_one_to_many -> {
@@ -52,6 +57,10 @@ class MainActivity : AppCompatActivity() {
 
             R.id.action_many_to_many -> {
                 getStudentWithCourse()
+                true
+            }
+            R.id.action_sort -> {
+                showSortingPopupMenu()
                 true
             }
 
@@ -93,6 +102,29 @@ class MainActivity : AppCompatActivity() {
         mainViewModel.getAllStudentWithCourse().observe(this) {
             Log.d(TAG, "getStudentWithCourse: $it")
             adapter.submitList(it)
+        }
+    }
+
+
+    private fun showSortingOptionMenu(isShow: Boolean) {
+        val view = findViewById<View>(R.id.action_sort) ?: return
+        view.visibility = if (isShow) View.VISIBLE else View.GONE
+    }
+    private fun showSortingPopupMenu() {
+        val view = findViewById<View>(R.id.action_sort) ?: return
+        PopupMenu(this, view).run {
+            menuInflater.inflate(R.menu.sorting_menu, menu)
+            setOnMenuItemClickListener {
+                mainViewModel.changeSortType(
+                    when (it.itemId) {
+                        R.id.action_ascending -> SortType.ASCENDING
+                        R.id.action_descending -> SortType.DESCENDING
+                        else -> SortType.RANDOM
+                    }
+                )
+                true
+            }
+            show()
         }
     }
 
